bf010db168a60ee1413de33c457dc38d12cef2b4
Yegor Levandovskyi <yegor.levandovskyi@interactio.io>
2021-10-15 21:48:05 +0300

    Lecture files

--- README.md
+++ README.md
@@ -1,35 +1,8 @@
-# First Lecture 
-Welcome to the first lecture!
+# Second lecture
+Let's start working on our project!
+
 ## Overview
-- What is NodeJS?
-- What is NPM?
-- Npm commands
-- Basic examples of node packages (fs, os, path)
-- How to create a node package?
-- What is JSON?
-- What is `package.json`?
-- ES6 module recap
-- `@types/node` package
+In this lecture we created a simple cli program that gets info from terminal input and then writes it to `db.json` file.
 
-## Steps
-- Install NodeJS
-- Open terminal
-- Run `npm install` or `npm i` (its the same thing) in this folder
-- Run `node index.js` 
-- See the result in terminal
-
-## How to create a NodeJS package?
-- Create a folder
-- Run `npm init -y` to create `package.json` with default configuration
-- Alternatively, you can run `npm init` without arguments, it will run cli that will questions about your package to generate a `package.json` file
-- Add `"type": "module"` to `package.json` to make use of ES6 Module syntax, otherwise you will have to use `commonjs` module syntax
-- VSCode tip: run `npm i @types/node -D` to add types to the project, it will make autocompletion work when importing bundled node modules
-
-## Links
-- [NodeJS Docs](https://nodejs.org/docs/latest-v15.x/api/)
-- [NPM Docs](https://docs.npmjs.com/)
-- [What is JSON?](https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Objects/JSON)
-- [More about package.json](https://docs.npmjs.com/cli/v7/configuring-npm/package-json)
-- [ES6 modules CAO](https://cao.lt/topic/js1/lesson/YNBPOxEAACQAuy_n)
-- [ES6 modules MDN](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Modules)
-- [CommonJS modules](https://nodejs.org/docs/latest/api/modules.html#modules_modules_commonjs_modules)
+## Thoughts? 🤔
+Currently our program does exactly what we want it to do. What do you think about the code? Does it read well? How do you think we can make it better? Will it scale well?
--- index.js
+++ index.js
@@ -1,15 +1,65 @@
-import { writeFileSync, readFileSync } from "fs";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
+import { createInterface } from "readline";
+import fs from "fs/promises";
 
-// write 'hello world' to 'test.txt' file
-writeFileSync("./test.txt", "hello world");
+// create readline interface to get user input
+const rl = createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
-// log the context of the 'test.txt' to the console
-console.log(readFileSync("./test.txt", "utf8"));
+// ask name, and then execute callback
+rl.question("What is your name?\n", (name) => {
+    // ask age, and then execute callback
+    rl.question("What is your age?\n", (age) => {
+        // create person object from user inputs
+        const person = {
+            name,
+            age
+        };
 
-// log path to current file to the console
-console.log(fileURLToPath(import.meta.url));
+        // read 'db.json' file
+        fs.readFile("./db.json", "utf8")
+        .then((data) => {
+            // create variable to save our data
+            let oldState;
 
-// log current directory name to the console
-console.log(dirname(fileURLToPath(import.meta.url)));
+            // check if there is any data in file, if not - write empty array
+            if (!data) {
+                // set saveState to empty array
+                oldState = [];
+
+                // write saveState to file
+                fs.writeFile("./db.json", JSON.stringify(oldState))
+            }
+
+            // set saveState to data from file
+            oldState = JSON.parse(data);
+
+            // append person to oldState and save the result to newState variable
+            const newState = [...oldState, person]
+
+            // write result to 'db.json' file
+            fs.writeFile("./db.json", JSON.stringify(newState))
+            .then(() => {
+                // log success message after file was saved
+                console.log("Saved person to 'db.json'")
+            })
+
+            // close readline
+            rl.close();
+        })
+        .catch((err) => {
+            // log that file doesn't exist yet
+            console.log("Look like 'db.json' doesn't exist yet. Creating...")
+
+            // this callback will execute if file doesn't exist
+            fs.writeFile("./db.json", JSON.stringify([person])).then(() => {
+                // log success message after file was saved
+                console.log("Saved person to 'db.json'")
+            })
+
+            // close readline
+            rl.close();
+        })
+    })
+});
