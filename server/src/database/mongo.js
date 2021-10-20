1cde952bb3a8aff3d161809dc36f16512bc981d9
Yegor Levandovskyi <yegor.levandovskyi@interactio.io>
2021-10-20 20:12:39 +0300

    Add comments

--- server/src/database/mongo.js
+++ server/src/database/mongo.js
@@ -2,20 +2,30 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
+// take user, password and cluster variables fields from env
 const {
     MONGO_USER,
-    MONGO_PW
+    MONGO_PW,
+    MONGO_CLUSTER
 } = process.env;
 
-const url = `mongodb+srv://${MONGO_USER}:${MONGO_PW}@cluster0.s8hnw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+// create url string
+const url = `mongodb+srv://${MONGO_USER}:${MONGO_PW}@${MONGO_CLUSTER}/myFirstDatabase?retryWrites=true&w=majority`;
 
+// create async context
 const main = async () => {
+    // try to connect to cluster
     try {
+        // create mongoose connection
         const connection = await mongoose.connect(url);
+
+        // log connection object
         console.log(connection);
     } catch (e) {
+        // if there is an error log it to the console
         console.error(e);
     }
 };
 
+// run program
 main();
